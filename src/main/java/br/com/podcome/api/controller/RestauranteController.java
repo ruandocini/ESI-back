fe17fc2ada7be36c6e0d4da3054ebeb3071ad7b4
ruandocini <ruandocini@gmail.com>
2021-11-01 13:29:57 -0300

    fix on cors

--- src/main/java/br/com/podcome/api/controller/RestauranteController.java
+++ src/main/java/br/com/podcome/api/controller/RestauranteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value="/api/v1/restaurantes")
 public class RestauranteController {
